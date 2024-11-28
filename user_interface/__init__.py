ce61af8cc4c481c8a96034471c8d42ca9348cbf6
John Belcher-Heath <john.belcher-heath@marineai.co.uk>
2024-11-28 12:28:23 +0000

    Created main structure for repo

---
+++ user_interface/__init__.py
@@ -0,0 +1,15 @@
+"""
+__init__.py
+    description
+Methods
+
+Attributes
+
+Dependencies
+
+"""
+
+__version__ = '0.0.0'
+__organization__ = 'MarineAI'
+__project__ = 'AIMS_racing'
+__tested__ = 'N'
