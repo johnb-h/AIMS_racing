a75a2585cf82e960539a15af6a8304f173ada525
John Belcher-Heath <john.belcher-heath@marineai.co.uk>
2025-01-14 13:47:23 +0000

    Main file lint QA improvements

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
-import argparse
+"""
+main.py
+    Main run file for AIMS racing application
+"""
 
+__version__ = '0.0.0'
+__project__ = 'aims_racing'
+__tested__ = 'N'
+
+# Standard Packages
+import argparse
+import sys
+
+# Relative Imports
 from user_interface.application_manager import ApplicationManager
 
 
-# Main Function
-def main(args):
+def main(args: argparse.Namespace) -> None:
+    """Executes main game engine"""
     application_manager = ApplicationManager(args.window_width, args.window_height)
     application_manager.run_game_loop()
 
 
-def parse_args():
+def parse_args(cli_args: list[str]) -> argparse.Namespace:
+    """
+    Parses CLI arguments for game execution
+    :param cli_args: Command Line Arguments
+    :type cli_args: list[str]
+    :return: Parsed arguments
+    :rtype: argparse.Namespace
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--window_width", type=int, default=1200)
     parser.add_argument("--window_height", type=int, default=800)
-    args = parser.parse_args()
+    args = parser.parse_args(cli_args)
     return args
 
 
 if __name__ == "__main__":
-    args = parse_args()
+    """Demo execution file"""
+    args = parse_args(sys.argv[1:])
     main(args)
